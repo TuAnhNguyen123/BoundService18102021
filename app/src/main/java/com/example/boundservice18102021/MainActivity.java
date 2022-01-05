54728bd94c0c2cb47a3aa01b465ae6dd6135e78c
JoseNguyen <abc@example.com>
2022-01-05 18:27:30 +0700

    update

--- app/src/main/java/com/example/boundservice18102021/MainActivity.java
+++ app/src/main/java/com/example/boundservice18102021/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.boundservice18102021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mbtnStartService, mbtnStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbtnStartService = findViewById(R.id.buttonStartService);
+        mbtnStopService = findViewById(R.id.buttonStopService);
+
+        mbtnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+            }
+        });
+
+        mbtnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
     }
 }
